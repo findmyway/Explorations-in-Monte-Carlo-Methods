c73d50cd743da4d18b463ff14354643d1acbd234
Tian Jun <tianjun@tianjun.me>
2017-07-13 10:04:42 +0800

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject emcm "0.1.0-SNAPSHOT"
+  :description "Clojure code of examples and problems in EMCM"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"
+            :year 2017
+            :key "mit"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])

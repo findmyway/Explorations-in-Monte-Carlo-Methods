2b562394117391e8632c19980ead3e545f5b2cea
Tian Jun <tianjun@tianjun.me>
2017-07-14 15:58:23 +0800

    update chapter 1

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "https://opensource.org/licenses/MIT"
             :year 2017
             :key "mit"}
+  :plugins  [[lein-gorilla "0.4.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.mikera/core.matrix "0.60.3"]])
+                 [net.mikera/core.matrix "0.60.3"]
+                 [lein-gorilla "0.4.0"]])
